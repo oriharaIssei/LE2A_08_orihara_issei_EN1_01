6f8958b8257b7c0d01ab4fb7738d6c37ecf35220
Amaimono <k023g0048@g.neec.ac.jp>
2024-04-15 12:12:19 +0900

    Init

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -13,6 +13,14 @@ public class GameManager : MonoBehaviour {
         }
         Debug.Log(debugText);
     }
+    int GetPlayerIndex(int[] intArray) {
+        for (int i = 0; i < intArray.Length; i++) {
+            if (i == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     // Start is called before the first frame update
     void Start() {
@@ -23,6 +31,7 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        playerIndex=GetPlayerIndex(map);
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             if (playerIndex > 0) {
                 map[playerIndex - 1] = 1;
